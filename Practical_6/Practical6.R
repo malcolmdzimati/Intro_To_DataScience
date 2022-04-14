01be9f4230f6c0ce27bb4967d21944fa505a2ecd
malcolmdzimati <malcolmdzimati@gmail.com>
2022-04-14 23:32:12 +0200

    Practical 67

---
+++ Practical_6/Practical6.R
@@ -0,0 +1,13 @@
+#Name : Batsirai Malcolm
+#Surname : Dzimati
+#Student number : u20456078
+
+
+library(ggplot2)
+library(readr)
+
+Peng <- read.table('~/GoogleDrive/2021/Semester1/WST212/Practicals/Practical_6/penguins.txt',header=TRUE,sep='\t')
+Infl <- read_csv('~/GoogleDrive/2021/Semester1/WST212/Practicals/Practical_6/appendicitis_influenza.csv')
+
+
+
